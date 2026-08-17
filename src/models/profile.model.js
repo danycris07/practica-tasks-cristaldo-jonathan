cba54111c4f833162c8f705e0042219534370adf
Cristaldo Jonathan <cristaldojonathandaniel2021@gmail.com>
2026-08-17 10:31:28 -0300

    Feat: se añadio las relaciones entre los modelos

---
+++ src/models/profile.model.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+const { DataTypes } = Sequelize;
+import { sequelize } from "../config/database.js";
+
+export const ProfileModel = sequelize.define("profile", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  bio: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
---
+++ src/models/tag.model.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize";
+const { DataTypes } = Sequelize;
+import { sequelize } from "../config/database.js";
+
+export const TagModel = sequelize.define("tag", {
+  id: { 
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true 
+  },
+  name: { 
+    type: DataTypes.STRING, 
+    allowNull: false, 
+    unique: true 
+  }
+});
